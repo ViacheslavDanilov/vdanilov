3005ef1ac77c9eae4bde5edd92c65eb315cb45a8
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-10-12 17:13:09 +0200

    [Fix]: simplify color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        'bg-dark': '#2A2A2A',     // deep background
-        'bg-light': '#F0F0F0',    // light background
-        'text-dark': '#2A2A2A',   // deep text color
-        'text-light': '#F0F0F0',  // light text color
+        'dark': '#2A2A2A',        // dark color
+        'light': '#F0F0F0',       // light color
         'accent': '#009BDF',      // accent color
         'selection': '#B3D6FF',   // text selection color
       },
