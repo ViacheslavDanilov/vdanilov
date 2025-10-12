48db65c9fd8f7296701aad4de345167e4f3ad3b6
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-10-12 16:17:16 +0200

    [Fature]: add initial HTML structure and Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0b1220',     // deep background
+        'light-bg': '#e6eef8',    // light background
+        'dark-text': '#0f1724',   // deep text color
+        'light-text': '#e6eef8',  // light text color
+        'highlight': '#3b82f6',   // brand / accent color (blue)
+      },
+    },
+  },
+}
