fbbb2ac1a0e11396c453370468fc4ff41a88a95b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-10-12 20:12:48 +0200

    style: remove quotes from color keys in tailwind.config.js for consistency

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -2,11 +2,11 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        'dark': '#2A2A2A',        // dark color
-        'light': '#F0F0F0',       // light color
-        'accent': '#009BDF',      // accent color
-        'selection': '#B3D6FF',   // text selection color
+        dark: "#2A2A2A",      // dark color
+        light: "#F0F0F0",  // light color
+        accent: "#009BDF",                    // accent color
+        selection: "#B3D6FF",                         // text selection color
       },
     },
   },
-}
+};
