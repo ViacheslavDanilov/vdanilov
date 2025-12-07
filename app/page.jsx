ed05726a44257cfb3825635687dd903908530163
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-12-08 00:14:23 +0100

    feat: add LightRays component for animated background effect and update dependencies

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,17 @@ import FeaturedProjects from "@/components/FeaturedProjects";
 import LogoGrid from "@/components/LogoGrid";
 import About from "@/components/About";
 import Statistics from "@/components/Statistics";
+import LightRays from "@/components/ui/light-rays";
 
 export default function Home() {
   return (
     <>
+      <div
+        className="absolute top-0 left-0 right-0 h-[60vh]"
+        style={{ zIndex: -1 }}
+      >
+        <LightRays />
+      </div>
       <div className="min-h-screen flex flex-col items-center pt-12 md:pt-36 gap-36 pb-32">
         <Hero />
         <About />
