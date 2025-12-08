f6dd70953ac87cfe9cd5e570b38ba284945af153
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-12-08 18:14:24 +0100

    fix: remove unnecessary padding from main layout for improved consistency

--- app/page.jsx
+++ app/page.jsx
@@ -9,14 +9,11 @@ import LightRays from "@/components/ui/light-rays";
 
 export default function Home() {
   return (
-    <>
-      <div
-        className="absolute top-0 left-0 right-0 h-[60vh]"
-        style={{ zIndex: -1 }}
-      >
+    <div className="relative min-h-screen pt-24">
+      <div className="fixed top-0 left-0 right-0 h-[60vh] pointer-events-none -z-10">
         <LightRays />
       </div>
-      <div className="min-h-screen flex flex-col items-center pt-12 md:pt-36 gap-36 pb-32">
+      <div className="flex flex-col items-center pt-12 md:pt-36 gap-36 pb-32">
         <Hero />
         <About />
         <Statistics />
@@ -25,6 +22,6 @@ export default function Home() {
         <LogoGrid />
         <CallToAction />
       </div>
-    </>
+    </div>
   );
 }
