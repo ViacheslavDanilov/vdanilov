f7b35eae8d6906a61b2c51b3e0084076775aa023
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2025-12-08 19:24:14 +0100

    fix: change LightRays div from fixed to absolute positioning

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ import LightRays from "@/components/ui/light-rays";
 export default function Home() {
   return (
     <div className="relative min-h-screen pt-24">
-      <div className="fixed top-0 left-0 right-0 h-[60vh] pointer-events-none -z-10">
+      <div className="absolute top-0 left-0 right-0 h-[60vh] pointer-events-none -z-10">
         <LightRays />
       </div>
       <div className="flex flex-col items-center pt-12 md:pt-36 gap-36 pb-32">
